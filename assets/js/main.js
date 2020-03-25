c8df85d037acd805b4a53e20909c345067365139
soroushshalali <soroushshalali@gmail.com>
2020-03-25 15:47:21 +0100

    v4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function searchFunc() {
     let input = document.getElementById("input").value;
-    console.log(input);
+    document.getElementById("contain").innerHTML = "";
     fetch(`https://newsapi.org/v2/everything?q=${input}&from=2020-02-25&sortBy=publishedAt&apiKey=fa168a8960ee4bf0911fbf6759ae54c7`)
         .then(response => response.json())
         .then(data => {
@@ -42,8 +42,10 @@ let cat;
 let btnCat = document.getElementsByClassName("btn-cat");
 for (let el of btnCat) {
     el.addEventListener("click", (elt) => {
+        document.getElementById("contain").innerHTML = "";
         cat = elt.target.innerText;
         main(cat);
+        console.log(cat);
 
     })
 }
